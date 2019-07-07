a3676490b317ea0e4f3ed8236fa2d9000d98fee3
mininerej <ejhong.4991@gmail.com>
2019-07-08 00:13:37 +0900

    feat: add introduction block

--- src/views/Home/Introduction.vue
+++ src/views/Home/Introduction.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    
-  </div>
+  <v-card>
+    <v-responsive class="introduction" :aspect-ratio="2.5/1">
+      <h2 class="subtitle">{{ $t("home.introduction.title") }}</h2>
+      <h3 class="content1">{{ $t("home.introduction.content") }}</h3>
+    </v-responsive>
+  </v-card>
 </template>
