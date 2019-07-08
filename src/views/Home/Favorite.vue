79c7224c129d6ee9f53d03cbe2d5da25989bccad
mininerej <ejhong.4991@gmail.com>
2019-07-08 23:28:44 +0900

    feat: add favorite box

--- src/views/Home/Favorite.vue
+++ src/views/Home/Favorite.vue
@@ -1,8 +1,27 @@
 <template>
-  <v-card>
     <v-responsive class="block favorite" :aspect-ratio="16/6">
-      <h2 class="subtitle">{{ $t("home.introduction.title") }}</h2>
-      <h3 class="content1">{{ $t("home.introduction.content") }}</h3>
+    <v-container justify-space-around>
+      <v-layout class="favorite-layout" row>
+          <v-flex class="text-xs-center favorite-flex" v-for="i in 3" :key="`4${i}`" xs4>
+            <img class="icon"  :src="require(`../../assets/images/favorite${i}.png`)" />
+            <h3 class="content1">{{ $t( `home.favorite.${i}.title` ) }}</h3>
+            <h4 class="content2 favorite-content">{{ $t( `home.favorite.${i}.content` ) }}</h4>
+          </v-flex>
+        </v-layout>
+    </v-container>
+
     </v-responsive>
-  </v-card>
 </template>
+<style lang="scss">
+  @media only screen and (max-width: 600px){
+    .favorite-layout{
+      flex-direction: column !important;
+    }
+    .favorite-flex{
+      max-width: 100% !important;
+    }
+  }
+  .favorite-content{
+    margin: 2em;
+  }
+</style>
--- src/views/Home/Introduction.vue
+++ src/views/Home/Introduction.vue
@@ -2,7 +2,14 @@
   <v-card>
     <v-responsive class="block introduction" :aspect-ratio="2.5/1">
       <h2 class="subtitle">{{ $t("home.introduction.title") }}</h2>
-      <h3 class="content1">{{ $t("home.introduction.content") }}</h3>
+      <h3 class="content1 introduction-content">{{ $t("home.introduction.content") }}</h3>
     </v-responsive>
   </v-card>
 </template>
+
+<style lang="scss">
+  .introduction-content {
+    margin: 1em;
+    padding: 0 2em;
+  }
+</style>
