e3b1278ccac04f544c19c7c32dc9c19be92b4e62
mininerej <ejhong.4991@gmail.com>
2019-07-08 21:15:59 +0900

    .

--- src/views/Home/Favorite.vue
+++ src/views/Home/Favorite.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    
-  </div>
+  <v-card>
+    <v-responsive class="block favorite" :aspect-ratio="16/6">
+      <h2 class="subtitle">{{ $t("home.introduction.title") }}</h2>
+      <h3 class="content1">{{ $t("home.introduction.content") }}</h3>
+    </v-responsive>
+  </v-card>
 </template>
--- src/views/Home/Introduction.vue
+++ src/views/Home/Introduction.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-responsive class="introduction" :aspect-ratio="2.5/1">
+    <v-responsive class="block introduction" :aspect-ratio="2.5/1">
       <h2 class="subtitle">{{ $t("home.introduction.title") }}</h2>
       <h3 class="content1">{{ $t("home.introduction.content") }}</h3>
     </v-responsive>
